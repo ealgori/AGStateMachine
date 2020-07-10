37abcca21ff89c913811a8bba5f36931b644c132
surgerer <surge_all@bk.ru>
2020-07-10 12:03:43 +0300

    Create StateMachine
    Invoke Func s on Event

---
+++ AGStateMachine/AGStateMachine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace AGStateMachine
+{
+    public abstract class AGStateMachine<TState, TEvent, TInstance>
+        where TState : Enum
+        where TEvent : Enum
+        where TInstance : class, IInstance<TState>
+
+    {
+        private ConcurrentDictionary<(TState,TEvent),Func<TInstance,Task>> _transtions = new ConcurrentDictionary<(TState, TEvent), Func<TInstance, Task>>();
+
+        private void  AddTransition(TState state, TEvent @event, Func<TInstance, Task> function)
+        {
+            if(function==null)
+                return;
+            _transtions[(state, @event)] = function;
+        }
+
+        private Task RaiseEvent(TEvent @event, TInstance instance)
+        {
+            if (!_transtions.ContainsKey((instance.CurrentState, @event)))
+                return Task.CompletedTask;
+
+            return _transtions[(instance.CurrentState, @event)].Invoke(instance);
+        }
+    }
+}
