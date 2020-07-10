dea8cd0ca26d8262e425bdb762e4764bf8d62639
surgerer <surge_all@bk.ru>
2020-07-10 13:07:08 +0300

    Add behavioural test

---
+++ AGStateMachine.Tests/Behavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace AGStateMachine.Tests
+{
+    
+    public class BehaviouralTests
+    {
+        [Fact]
+        public async Task ShouldInvokeFuncOneTime()
+        {
+            var sm = TestSM1.Create();
+            var stateInstance = new TestSM1.TestStateInstance();
+            var mockFunc = new Mock<Func<TestSM1.TestStateInstance,Task>>();
+            
+            sm.AddTransition(
+                TestSM1.StateSM1.First, 
+                TestSM1.EventSM1.FirstEvent,
+                mockFunc.Object
+            );
+
+            await sm.RaiseEvent(TestSM1.EventSM1.FirstEvent, stateInstance);
+
+            mockFunc.Verify(x=>x(stateInstance), Times.Once);
+            
+        }
+       
+    }
+}
