1ec8de45c5852626e818154ef850a6a417bdbe84
surgerer <surge_all@bk.ru>
2020-08-17 21:08:55 +0300

    add ShouldSaveInvokeSequence test

--- AGStateMachine.Tests/Behavior.cs
+++ AGStateMachine.Tests/Behavior.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Moq;
 using Xunit;
 
 namespace AGStateMachine.Tests
 {
-    
     public class BehaviouralTests
     {
         [Fact]
@@ -13,36 +13,62 @@ namespace AGStateMachine.Tests
         {
             var sm = TestSM1.Create();
             var stateInstance = new TestSM1.TestStateInstance();
-            var mockFunc = new Mock<Func<TestSM1.TestStateInstance,Task>>();
-            
+            var mockFunc = new Mock<Func<TestSM1.TestStateInstance, Task>>();
+
             sm.AddTransition(
-                TestSM1.StateSM1.First, 
+                TestSM1.StateSM1.First,
                 TestSM1.EventSM1.FirstEvent,
                 mockFunc.Object
             );
 
             await sm.RaiseEvent(TestSM1.EventSM1.FirstEvent, stateInstance);
 
-            mockFunc.Verify(x=>x(stateInstance), Times.Once);
-            
-        } 
+            mockFunc.Verify(x => x(stateInstance), Times.Once);
+        }
+
         [Fact]
         public async Task ShouldNotInvokeFunc()
         {
             var sm = TestSM1.Create();
             var stateInstance = new TestSM1.TestStateInstance();
-            var mockFunc = new Mock<Func<TestSM1.TestStateInstance,Task>>();
-            
+            var mockFunc = new Mock<Func<TestSM1.TestStateInstance, Task>>();
+
             sm.AddTransition(
-                TestSM1.StateSM1.Second, 
+                TestSM1.StateSM1.Second,
                 TestSM1.EventSM1.FirstEvent,
                 mockFunc.Object
             );
 
             await sm.RaiseEvent(TestSM1.EventSM1.FirstEvent, stateInstance);
 
-            mockFunc.Verify(x=>x(stateInstance), Times.Never);
+            mockFunc.Verify(x => x(stateInstance), Times.Never);
         }
-       
+
+
+        [Fact]
+        public async Task ShouldSaveInvokeSequence()
+        {
+            var sm = TestSM1.Create();
+            var stateInstance = new Mock<TestSM1.TestStateInstance>(MockBehavior.Strict);
+            var sequence = new MockSequence();
+            stateInstance.InSequence(sequence).Setup(i => i.Invoke(1));
+            stateInstance.InSequence(sequence).Setup(i => i.Invoke(2));
+            var typedEvent1 = new TestSM1.TypedEvent(){Counter = 1};
+            var typedEvent2 = new TestSM1.TypedEvent(){Counter = 2};
+            sm.AddTransition<TestSM1.TypedEvent>(
+                TestSM1.StateSM1.First,
+                (inst, ev) =>
+                {
+                    inst.Invoke(ev.Counter);
+                    return Task.CompletedTask;
+                }
+            );
+
+            await sm.RaiseEvent(typedEvent1, stateInstance.Object);
+            await sm.RaiseEvent(typedEvent2, stateInstance.Object);
+            
+            stateInstance.Verify(i=>i.Invoke(1));
+            stateInstance.Verify(i=>i.Invoke(2));
+        }
     }
 }
