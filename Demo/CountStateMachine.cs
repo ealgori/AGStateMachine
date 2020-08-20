46d0b39af943857b15f05e2a6baba98effb799af
surgerer <surge_all@bk.ru>
2020-08-20 15:19:23 +0300

    add Demo project

---
+++ Demo/CountStateMachine.cs
@@ -0,0 +1,31 @@
+﻿using AGStateMachine;
+
+namespace Demo
+{
+        public enum States
+        {
+                A,B,C
+        }
+        
+        public enum Events
+        {
+                M,N
+        }
+
+        public class StateInstance:IInstance<States>
+        {
+                public int Counter { get; set; }
+                public States CurrentState { get; set; }
+        }
+
+        public class TypedEvent
+        {
+                public int Count { get; }
+
+                public TypedEvent(int count)
+                {
+                        Count = count;
+                }
+        }
+        public class CountStateMachine:AGStateMachine<States,Events,StateInstance>{}
+}
